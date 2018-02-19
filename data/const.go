966050da610a00af61df84556d6eb9f342cb957f
Junhwan Beom <solkblte@icloud.com>
2018-02-19 20:10:49 +0900

    Add description of const.go

--- data/const.go
+++ data/const.go
@@ -1,9 +1,11 @@
 package data
 
+// Username makes you change username easiler
 func Username() string {
 	return "yourusername"
 }
 
+// Password makes you change passowrd easiler.
 func Password() string {
 	return "yourpassword"
 }
